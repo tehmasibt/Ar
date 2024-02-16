4f0c1684f4facb6f6e684a7d1cdea818cba8e7a7
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-16 14:08:28 +0400

    A

---
+++ arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region task-1
+//int[] number = { 5, 4, 6, 2, 17, 10 };
+//int sum=0;
+//for (int i = 0; i < number.Length; i++)
+//{
+//    sum += number[i];
+//}
+//Console.WriteLine(sum);
+#endregion
